542849ad379b3ae2c4d6be16bc855e827df1f244
imotyashok <=imotyashok@luc.edu>
2020-12-06 00:11:59 -0600

    ran model on test set

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ print("Test set shape:" + str(test.shape))
 
 #-------------------------------------- BEGIN MODEL TESTING HERE ----------------------------------------------------
 
-print("\n>>> Beginning model testing...")
+print("\n>>> Beginning model testing on validate data set...")
 print("Model 1: Logistic Regression, C=1, penalty=l1")
 lr = LogisticRegression(penalty='l1', C=1.0, random_state=1, solver='liblinear', multi_class='ovr')
 lr.fit(train_x, train_y)
@@ -100,4 +100,12 @@ svm.fit(train_x, train_y)
 accuracy = svm.score(validate_x, validate_y)
 print("Accuracy: %.3f" % accuracy)
 
-print("\n>>> Results: Logistic Regression and SVM give very similar results; best model is most likely Model 1")
+print("\n>>> Model Analysis: Logistic Regression and SVM give very similar results; best model is most likely Model 1")
+print("\n>>> Testing our best model on test data set:")
+lr = LogisticRegression(penalty='l1', C=1.0, random_state=1, solver='liblinear', multi_class='ovr')
+lr.fit(train_x, train_y)
+accuracy = lr.score(test_x, test_y)
+print("Accuracy: %.3f" % accuracy)
+
+print("\nResults: Our model is around 90% accurate at predicting whether someone votes Democrat or Republican."+
+      "\nThis model seems fairly reliable and well fitted, since the validation and test set accuracy are both around 90%.")
